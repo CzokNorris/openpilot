f2b1873bea4c93f128e9fbb48851b69d49660d03
CzokNorris <czoknorris@me.com>
2021-11-18 14:57:18 +0100

    Update car_helpers.py
    Bugfix

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -15,7 +15,7 @@ EventName = car.CarEvent.EventName
 
 def get_startup_event(car_recognized, controller_available, fuzzy_fingerprint, fw_seen):
   #if comma_remote and tested_branch:
-    event = EventName.startup
+  event = EventName.startup
   #else:
    # event = EventName.startupMaster
 
